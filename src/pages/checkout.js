dc681a4081299682d43eb4c1e5bc880f2a5f21bc
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-08-17 22:23:52 +0100

    worked on mobile view, deleted some code snippets as well

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,4 +1,3 @@
-import CheckoutSummary from '@/components/CheckoutSummary';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
@@ -6,11 +5,6 @@ export default function Checkout() {
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
-    country: '',
-    address: '',
-    city: '',
-    state: '',
-    zip: '',
     phone: '',
     email: '',
   });
@@ -29,9 +23,9 @@ export default function Checkout() {
 
   return (
     <section className="checkout my-16 mx-8">
-      <form onSubmit={handleFormSubmit} className="flex items-start justify-between max-w-[1200px] mx-auto">
-        <div className="formFill">
-          <div className="flex items-center gap-10">
+      <form onSubmit={handleFormSubmit} className="flex flex-col xl:flex-row items-start justify-between max-w-[1200px] mx-auto">
+        <div className="formFill mx-auto xl:mx-0 mb-16 xl:mb-0">
+          <div className="flex flex-col xl:flex-row items-center gap-5 xl:gap-10 mb-8 xl:mb-0">
             <div>
               <label htmlFor="firstName" className="mb-2 block">
                 First name
@@ -47,57 +41,24 @@ export default function Checkout() {
             </div>
           </div>
 
-          <div className="my-8">
-            <label htmlFor="country" className="mb-2 block">
-              Country / Region
-            </label>
-            <input type="text" name="country" value={formData.country} onChange={handleInputChange} required className="outline-none w-[300px] p-3 border border-gray-300" placeholder="Country" />
-          </div>
+          <div className="flex flex-col xl:flex-row items-center gap-5 xl:gap-10">
+            <div className="xl:my-8">
+              <label htmlFor="phone" className="mb-2 block">
+                Phone
+              </label>
+              <input type="text" name="phone" value={formData.phone} onChange={handleInputChange} required className="outline-none w-[300px] p-3 border border-gray-300" placeholder="Phone" />
+            </div>
 
-          <div className="my-8">
-            <label htmlFor="address" className="mb-2 block">
-              Street Address
-            </label>
-            <input type="text" name="address" value={formData.address} onChange={handleInputChange} required className="outline-none w-[300px] p-3 border border-gray-300" placeholder="Address" />
-          </div>
-
-          <div className="my-8">
-            <label htmlFor="city" className="mb-2 block">
-              Town / City
-            </label>
-            <input type="text" name="city" value={formData.city} onChange={handleInputChange} required className="outline-none w-[300px] p-3 border border-gray-300" placeholder="City" />
-          </div>
-
-          <div className="my-8">
-            <label htmlFor="state" className="mb-2 block">
-              State / County
-            </label>
-            <input type="text" name="state" value={formData.state} onChange={handleInputChange} required className="outline-none w-[300px] p-3 border border-gray-300" placeholder="State" />
-          </div>
-
-          <div className="my-8">
-            <label htmlFor="zip" className="mb-2 block">
-              Postcode / ZIP
-            </label>
-            <input type="text" name="zip" value={formData.zip} onChange={handleInputChange} required className="outline-none w-[300px] p-3 border border-gray-300" placeholder="Zip" />
-          </div>
-
-          <div className="my-8">
-            <label htmlFor="phone" className="mb-2 block">
-              Phone
-            </label>
-            <input type="text" name="phone" value={formData.phone} onChange={handleInputChange} required className="outline-none w-[300px] p-3 border border-gray-300" placeholder="Phone" />
-          </div>
-
-          <div className="my-8">
-            <label htmlFor="email" className="mb-2 block">
-              Email Address
-            </label>
-            <input type="text" name="email" value={formData.email} onChange={handleInputChange} required className="outline-none w-[300px] p-3 border border-gray-300" placeholder="Email" />
+            <div className="xl:my-8">
+              <label htmlFor="email" className="mb-2 block">
+                Email Address
+              </label>
+              <input type="email" name="email" value={formData.email} onChange={handleInputChange} required className="outline-none w-[300px] p-3 border border-gray-300" placeholder="Email" />
+            </div>
           </div>
         </div>
 
-        <div className="checkoutSummary bg-[#F5F5F5] w-[30%] px-6 py-3">
+        <div className="checkoutSummary bg-[#F5F5F5] w-[300px] md:w-[480px] mx-auto xl:mx-0 xl:w-[30%] px-6 py-3">
           <h1 className="text-xl text-black font-bold">Your Order</h1>
           <p className="my-3 text-[500]">PDFname x 1</p>
           <p>
@@ -121,8 +82,6 @@ export default function Checkout() {
           </Link>
         </div>
       </form>
-
-      <CheckoutSummary />
     </section>
   );
 }
